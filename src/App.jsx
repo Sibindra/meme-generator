6cd4fb507bee2a97ffd7e6c7ea4615f995480ba4
Sibindra <timalsinasibndra@gmail.com>
2022-11-20 10:56:17 +0545

    added react boostrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+
+import { Button, Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <Card className="rounded-0 p-vh-3">
+      <Header/>
+    </Card>
+  //  <div id="hero">
+  //  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <div id="Header">
+            <h2 id="title">
+                Meme Generator
+            </h2>
+
+        </div>
+    )
+}
