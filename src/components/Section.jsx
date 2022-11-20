cc075827f7d45156a1560bfdb8cd5490e0269d3c
Sibindra <timalsinasibndra@gmail.com>
2022-11-20 13:01:12 +0545

    condenderium

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 
-import memesData from "../memesData";
 
 
-export default function Section() {
-    function getmemeImage() {
-        let url;
-        const memeArray = memesData.data.memes;
-        const randomNo = Math.floor(Math.random() * memeArray.length);
-        url = memeArray[randomNo].url;
-        console.log(url);
-    }
+export default function Section(props) {
 
-    document.getElementById('btn').onclick=getmemeImage;
-
-
-    return <div id="section">works</div>;
+    return <div id="section">{props.url}</div>;
 }
