0cc8fa91a2fa4e7febbc47d1c7140158c4a8be6d
Sibindra <timalsinasibindra@gmail.com>
2022-12-01 09:01:23 +0545

    section text

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-
-
 export default function Section(props) {
+    return (
+        <div id="section">
+            <img src={props.url} alt={props.url} id="img" />
 
-    return <div id="section">
-        <img src={props.url} alt={props.url} id="img"/>
-    </div>;
+
+            <h2 id="topText" className="memeText">Shut Up</h2>
+            <h2 id="bottomText" className="memeText">and Take my money</h2>
+
+        </div>
+    );
 }
